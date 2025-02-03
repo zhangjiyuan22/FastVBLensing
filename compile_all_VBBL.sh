1e42fe62bf0116ee10cd08a46f3898d2f112dfb9
zhangjiyuan22 <117197511+zhangjiyuan22@users.noreply.github.com>
2025-02-03 18:10:33 +0800

    Update compile_all_VBBL.sh

--- compile_all_VBBL.sh
+++ compile_all_VBBL.sh
@@ -1,9 +1,10 @@
-#Check for the existence of the dynamic library path environment variable(LD_LIBRARY_PATH)
-echo $LD_LIBRARY_PATH
-#If there is nothing to be displayed, add a default path value (or not if you wish to)
-LD_LIBRARY_PATH=/usr/local/lib
-#add the desired path (need to change the following path according to the location of this folder in your system)
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/work/zhangjiyuan/VBMicrolensing_and_VBBL/bin/
+###Check for the existence of the dynamic library path environment variable(LD_LIBRARY_PATH)
+#echo $LD_LIBRARY_PATH
+###If there is nothing to be displayed, add a default path value (or not if you wish to)
+#LD_LIBRARY_PATH=/usr/local/lib
+###add the desired path (need to change the following path according to the location of this folder in your system)
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/work/zhangjiyuan/FastVBLensing/bin/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:bin/
 
 
 
@@ -29,20 +30,20 @@ g++ -O3 -g -Wall -Wextra -march=native test_VBBLNoOptimization.cpp -Lbin -l_VBBi
 
 
 
-##############################
-### optimized version VBBL ###
-##############################
+###########################################
+### Compiling Optimization version VBBL ###
+###########################################
 
 ### build a dynamic library(.a is static link, .so is dynamic/runtime link)
-rm -rf bin/lib_VBBinaryLensingLibraryOptimized.so
-g++ -fPIC -O3 -g -flto -Wall -Wextra -shared -march=skylake-avx512 -o bin/lib_VBBinaryLensingLibraryOptimized.so VBBL_lib_optimized/VBBinaryLensingLibrary_v3p6.cpp
-chmod -x bin/lib_VBBinaryLensingLibraryOptimized.so
+rm -rf bin/lib_VBBinaryLensingLibraryCompilingOptimization.so
+g++ -fPIC -O3 -g -flto -Wall -Wextra -shared -march=native -o bin/lib_VBBinaryLensingLibraryCompilingOptimization.so VBBL_lib_compiling_optimization/VBBinaryLensingLibrary_v3p6.cpp
+chmod -x bin/lib_VBBinaryLensingLibraryCompilingOptimization.so
 
 
 ### build the test code which calls the dynamic library
-rm -rf bin/test_VBBLOptimized.out
+rm -rf bin/test_VBBLCompilingOptimization.out
 #the source file should be in front of the dynamic library
-g++ -O3 -g -Wall -Wextra -march=skylake-avx512 test_VBBLOptimized.cpp -Lbin -l_VBBinaryLensingLibraryOptimized -o bin/test_VBBLOptimized.out
+g++ -O3 -g -Wall -Wextra -march=native test_VBBLCompilingOptimization.cpp -Lbin -l_VBBinaryLensingLibraryCompilingOptimization -o bin/test_VBBLCompilingOptimization.out
 
 
 
@@ -53,20 +54,18 @@ g++ -O3 -g -Wall -Wextra -march=skylake-avx512 test_VBBLOptimized.cpp -Lbin -l_V
 
 
 
-##############################
-### 4.4 version VBBL ###
-##############################
+#######################################################
+### Algorithmic Compiling Optimization version VBBL ###
+#######################################################
 
 ### build a dynamic library(.a is static link, .so is dynamic/runtime link)
-rm -rf bin/lib_VBBinaryLensingLibraryV4p4.so
-g++ -fPIC -O3 -g -flto -Wall -Wextra -shared -march=skylake-avx512 -o bin/lib_VBBinaryLensingLibraryV4p4.so VBBL_lib_v4p4/VBBinaryLensingLibrary_v3p6.cpp
-#g++ -fPIC -O1 -g -flto -Wall -Wextra -shared -o bin/lib_VBBinaryLensingLibraryV4p3.so VBBL_lib_v4p3/VBBinaryLensingLibrary_v3p6.cpp
-chmod -x bin/lib_VBBinaryLensingLibraryV4p4.so
+rm -rf bin/lib_VBBinaryLensingLibraryAlgorithmicCompilingOptimization.so
+g++ -fPIC -O3 -g -flto -Wall -Wextra -shared -march=native -o bin/lib_VBBinaryLensingLibraryAlgorithmicCompilingOptimization.so VBBL_lib_algorithmic_compiling_optimization/VBBinaryLensingLibrary_v3p6.cpp
+chmod -x bin/lib_VBBinaryLensingLibraryAlgorithmicCompilingOptimization.so
 
 
 ### build the test code which calls the dynamic library
-rm -rf bin/test_VBBLV4p4.out
+rm -rf bin/test_VBBLAlgorithmicCompilingOptimization.out
 #the source file should be in front of the dynamic library
-g++ -O3 -g -Wall -Wextra -march=skylake-avx512 test_VBBLV4p4.cpp -Lbin -l_VBBinaryLensingLibraryV4p4 -o bin/test_VBBLV4p4.out
-#g++ -O1 -g -Wall -Wextra test_VBBLV4p3.cpp -Lbin -l_VBBinaryLensingLibraryV4p3 -o bin/test_VBBLV4p3.out
+g++ -O3 -g -Wall -Wextra -march=native test_VBBLAlgorithmicCompilingOptimization.cpp -Lbin -l_VBBinaryLensingLibraryAlgorithmicCompilingOptimization -o bin/test_VBBLAlgorithmicCompilingOptimization.out
 
